576abea29132fe1bfb1f00974190615c8a7aa1b9
elind6067 <elind6067@gmail.com>
2025-10-06 15:01:08 -0500

    removed header definitions

--- gates.h
+++ gates.h
@@ -1,5 +1,5 @@
-#ifndef GATES_H
-#define GATES_H
+/*#ifndef GATES_H
+#define GATES_H*/
 /*
     function prototypes for gates.c
 */
@@ -10,4 +10,4 @@ int NAND(int a, int b);
 int NOR(int a, int b);
 int XOR(int a, int b);
 int XNOR(int a, int b);
-#endif // GATES_H  
+//#endif // GATES_H  
