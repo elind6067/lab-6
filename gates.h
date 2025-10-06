b150dfd18a3117cb52af18652218e7d960ab84f9
elind6067 <elind6067@gmail.com>
2025-10-06 13:46:45 -0500

    added function prototypes to header file

---
+++ gates.h
@@ -0,0 +1,10 @@
+/*
+    function prototypes for gates.c
+*/
+int NOT(int a);
+int AND(int a, int b);
+int OR(int a, int b);
+int NAND(int a, int b);
+int NOR(int a, int b);
+int XOR(int a, int b);
+int XNOR(int a, int b);
